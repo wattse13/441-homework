29450fa5635230e6c3e158b1fe4558013e020faf
Eli Watts <eliwatts13@gmail.com>
2020-01-21 12:45:51 -0700

    Create script.js document and added content

---
+++ hw-2/script.js
@@ -0,0 +1,20 @@
+/*
+Author: Eli Watts
+File Name: script.js
+Date: 21/01/2020
+Purpose: To Print Superlatives to Web Console
+*/
+
+//Uses the log method of the console object to write a string of text on the web page console
+console.log('Favorite Website #1: The Onion');
+console.log('Favorite Website #2: YouTube');
+console.log('Favorite Website #3: Electric Literature');
+
+console.log('Favorite Game #1: Hollow Knight');
+console.log('Favorite Game #2: Dark Souls');
+console.log('Favorite Game #3: Bioshock');
+console.log('Favorite Game #4: Nier Automata');
+
+console.log('Favorite Artist #1: Shirley Jackson');
+console.log('Favorite Artist #2: Zdzisław Beksiński');
+console.log('Favorite Artist #3: Kara Walker');
