91a1658ffe67e2234b1c0a561b03c29ceb9b22eb
Eli Watts <eliwatts13@gmail.com>
2020-02-21 08:56:27 -0700

    Created function to story player information

--- hw-6/json.js
+++ hw-6/json.js
@@ -4,3 +4,14 @@ File Name: memory.html
 Date: 12/02/2020
 Purpose: MART 441 Homework Assignment #6
 */
+
+function addPlayerInfo() {
+  let nameOne = document.getElementById("firstNameField").value;
+  let nameTwo = document.getElementById("lastNameField").value;
+  let age = document.getElementById("ageField").value;
+  let clickNumber = document.getElementById("clickAttempts");
+  let playerInfo = { "firstName": nameOne , "lastName": nameTwo , "playerAge": age , "playerAttempts": clickNumber };
+
+  localStorage.setItem("playerInfo", JSON.stringify(playerInfo));
+  console.log(playerInfo);
+}
