d2905cba223c75386ff9e86040052c7dc50888cc
Eli Watts <eliwatts13@gmail.com>
2020-03-04 10:55:23 -0700

    Deleted then remade jqueryArt.js file

---
+++ hw-8/script/jqueryArt.js
@@ -0,0 +1,23 @@
+/*
+Author: Eli Watts
+File Name: jqueryArt.js
+Date: 12/02/2020
+Purpose: MART 441 Homework Assignment #8
+*/
+
+console.log("hi!");
+
+
+
+/******************************************************************************/
+//Code Grave Yard
+/******************************************************************************/
+
+//Wanted to make code that would rotate text/squre
+/*$(document).ready( function(){
+  $( "#btnSubmit" ).click( function() {
+    $( "#side1" ).css("transform", "rotate(-90deg)", function(){
+      $( "#text2" ).toggle();
+    });
+  });
+});*/
