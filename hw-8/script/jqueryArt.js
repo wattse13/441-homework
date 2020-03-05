d01502f8c33fc5b63dca98dbff91fe00bcf95608
Eli Watts <eliwatts13@gmail.com>
2020-03-05 13:32:04 -0700

    Added content to jqueryArt.js

--- hw-8/script/jqueryArt.js
+++ hw-8/script/jqueryArt.js
@@ -5,20 +5,50 @@ Date: 12/02/2020
 Purpose: MART 441 Homework Assignment #8
 */
 
-$(document).ready(function () {
-            $( "#btnSubmit" ).click(function () {
-                 $( "#motherShip" ).fadeOut( "slow" , function(){
-                     $( "#rocketShip" ).attr( "src" , "../images/rocketShip.png" );
-                     console.log("here!");
-                 });
-            });
+$( document ).ready(function () {
+  $( "#btnSubmit" ).click(function () {
+    $( "#motherShip" ).fadeOut( "slow" , function() {
+      $( "#secondNight" ).fadeIn( "slow" , function() {
+        $( "#rocketShip" ).fadeIn( "fast" ).animate( { top: -1000 } , "slow" ).fadeOut( "fast" , function() {
+          $( "body" ).css( {"overflow-y": "hidden"} );
         });
+      });
+    });
+  });
+});
 
 
 
 /******************************************************************************/
 //Code Grave Yard
 /******************************************************************************/
+
+/*$( document ).ready(function () {
+  $( "#btnSubmit" ).click(function () {
+    $( "#motherShip" ).fadeOut( "slow" , function(){
+      $( "#rocketShip" ).fadeIn( "fast" ).animate( { top: -1000 } , "slow" ).fadeOut( "fast"  , function() {
+         $( "body" ).css( {"overflow-y": "hidden"} , function() {
+           $( "#secondNight" ).fadeIn( "slow" );
+            console.log("hi!");
+         });
+      });
+    });
+  });
+});*/
+
+/*$(document).ready(function () {
+            $( "#btnSubmit" ).click(function () {
+                 $( "#motherShip" ).fadeOut( "slow" , function(){
+                   $( "#secondNight" ).fadeIn( "fast" , function(){
+                     $( "body" ).css( "overflow-y" , "hidden" , function(){
+                       $( "#rocketShip" ).fadeIn( "fast" , function(){
+                         console.log("here!");
+                       });
+                     });
+                  });
+               });
+            });
+        });*/
 
 //Wanted to make code that would rotate text/squre
 /*$(document).ready( function(){
