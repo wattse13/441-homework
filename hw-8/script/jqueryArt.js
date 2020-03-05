844d027fe95ce71938f9476e25b413ea313625c6
Eli Watts <eliwatts13@gmail.com>
2020-03-05 15:09:46 -0700

    things do things!

--- hw-8/script/jqueryArt.js
+++ hw-8/script/jqueryArt.js
@@ -5,12 +5,25 @@ Date: 12/02/2020
 Purpose: MART 441 Homework Assignment #8
 */
 
+var para1Fade = $( "paragraph1" ).fadeOut( "slow" );
+
 $( document ).ready(function () {
   $( "#btnSubmit" ).click(function () {
     $( "#motherShip" ).fadeOut( "slow" , function() {
       $( "#secondNight" ).fadeIn( "slow" , function() {
         $( "#rocketShip" ).fadeIn( "fast" ).animate( { top: -1000 } , "slow" ).fadeOut( "fast" , function() {
-          $( "body" ).css( {"overflow-y": "hidden"} );
+          $( "#paragraph1" ).fadeIn( "slow" );
+        });
+      });
+    });
+  });
+  $( "#paragraph1" ).click(function() {
+    $( "#paragraph1" ).fadeOut( "slow" , function() {
+      $( "#paragraph2" ).fadeIn( "slow" , function() {
+        $( "#paragraph2" ).click(function(){
+          $( "#paragraph2" ).fadeOut( "slow" , function(){
+            $( "#textBox3" ).fadeIn( "slow" );
+          });
         });
       });
     });
