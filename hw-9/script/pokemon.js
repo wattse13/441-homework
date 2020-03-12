0ac138527f1e6f04eef8518dfb613ceedbc8d5cf
Eli Watts <eliwatts13@gmail.com>
2020-03-12 16:30:16 -0600

    Set up live server

--- hw-9/script/pokemon.js
+++ hw-9/script/pokemon.js
@@ -5,13 +5,65 @@ Date: 03/10/2020
 Purpose: MART 441 Homework Assignment #9
 */
 
+//var pokemonInfo = [ { "pokeName": "" , "pokePicture": "" , "pokeType": "" } ];
+var pokeInfo = new Array();
+
+class PokeDisplayer {
+  constructor( pokeName , pokePicture , pokeType ) {
+    this.pokeName = pokeName;
+    this.pokePicture = pokePicture;
+    this.pokeType = pokeType;
+  }
+  get nabName() {
+    return this.pokeName;
+  }
+  get nabPicture() {
+    return this.pokePicture;
+  }
+  get nabType() {
+    return this.pokeType;
+  }
+}
+
+
+
 $(document).ready(function () {
+            $( "#startButton" ).click(function () {
+              $.getJSON( "../script/pokedex.json" , function( result ) {
+                //window.alert( result.pokemon[0].name );
+                $.each( JSON.parse( result.pokemon[0].name ) , function( ) {
+                  console.log( result.pokemon[0].name );
+            });
+        });
+    });
+});
+
+
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+// Code Grave Yard
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+/*$(document).ready(function () {
             $("#startButton").click(function () {
-                $("#pokemonInformation").load("pokedex.json" , function(responseText) {
-                  window.alert(responseText);
-                  $( "#startButton" ).fadeOut( "slow" , function() {
-                    $( "#backForward" ).fadeIn( "slow" );
+              $("#pokemonInformation").load("../script/pokedex.json" , function(responseText) {
+                window.alert(responseText);
+                $( "#startButton" ).fadeOut( "slow" , function() {
+                  $( "#backForward" ).fadeIn( "slow" );
                   });
                 });
             });
-        });
+        });*/
+/*function newJson() {
+  var myNewJson = new PokeDisplayer( { "pokeName": "this.pokeName" , "pokePicture": "this.pokePicture" , "pokeType": "this.pokeType" } );
+  pokeInfo.push( myNewJson );
+}*/
+
+/*function clickCounter() {
+  var start = 0;
+  if ( $( "#nextButton" ).click == true ) {
+    start ++;
+  } else if ( $( "#previousButton" ).click == true ) {
+    start --;
+  }
+}*/
