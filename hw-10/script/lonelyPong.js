1912c01b18742ff6b57d5cef0b892a008ae6415a
Eli Watts <eliwatts13@gmail.com>
2020-03-30 15:39:07 -0600

    Things sorta work!

--- hw-10/script/lonelyPong.js
+++ hw-10/script/lonelyPong.js
@@ -50,34 +50,87 @@ class LonelyPongPlayers {
     set nabColor(color) {
       this.color = color;
     }
-    drawBlock() {
-      var canvas = document.getElementById("myCanvas");
-      var ctx = canvas.getContext("2d");
-      ctx.fillStyle = this.color;
-      //ctx.clearRect( 0 , 0 , 600 , 400 );
-      ctx.fillRect( this.x , this.y , this.height , this.width );
-      setInterval( this.update , 1000 );
-    }
-    update() {
-      this.y +=10;
-      this.drawBlock();
-    }
+
 }
 
 $(document).ready(function(){
   $( "#startButton" ).click(function () {
+    $(this).keypress(function(event){
+        getKey(event);
+    });
 
-    var moveSpeed = 10;
-    var player1 = new LonelyPongPlayers( 50 , 100 , 200 , 20 , moveSpeed , "#0000FF" );
-    //var ghost1 = new LonelyPongPlayers( 400 , 50 , 50 , 50 , 0 , "black" );
+    var moveSpeed = 5;
+    var player1 = new LonelyPongPlayers( 50 , 175 , 50 , 50 , moveSpeed , "purple" );
+    var ghost1 = new LonelyPongPlayers( 500 , 175 , 50 , 50 , 0 , "grey" );
 
-    player1.update();
-    //player1.drawBlock();
+    drawBlock();
+    setInterval( update , 1000/60 );
 
 
-    //player1.getKey(event);
-    //ghost1.drawBlock();
+    function drawBlock() {
+      var canvas = document.getElementById("myCanvas");
+      var ctx = canvas.getContext("2d");
+      ctx.clearRect( 0 , 0 , 600 , 400 );
+      ctx.fillStyle = player1.color;
+      ctx.fillRect( player1.x , player1.y , player1.height , player1.width );
+      ctx.fillStyle = ghost1.color;
+      ctx.fillRect( ghost1.x , ghost1.y , ghost1.height , ghost1.width );
 
+    }
+
+    function update() {
+      //player1.y +=10;
+      drawBlock();
+    }
+
+    function getKey(event) {
+      var char = event.which || event.keyCode;
+      var actualLetter = String.fromCharCode(char);
+      var ouch = hasCollided( player1 , ghost1 );
+
+      if(actualLetter == "w") {
+        moveUp();
+      } else if(actualLetter == "s" ) {
+        moveDown();
+      } else if(actualLetter == "a" ) {
+        moveLeft();
+      } else if(actualLetter == "d" ) {
+        moveRight();
+      }
+      if( ouch ) {
+        console.log("ouch");
+        rude();
+      }
+    }
+    function moveUp() {
+      player1.y -= moveSpeed;
+    }
+
+    function moveDown() {
+      player1.y += moveSpeed;
+    }
+    function moveLeft() {
+      player1.x -= moveSpeed;
+    }
+    function moveRight() {
+      player1.x += moveSpeed;
+    }
+
+    function hasCollided(object1, object2) {
+    return !(
+        ((object1.y + object1.height) < (object2.y)) ||
+        (object1.y > (object2.y + object2.height)) ||
+        ((object1.x + object1.width) < object2.x) ||
+        (object1.x > (object2.x + object2.width))
+      );
+    }
+
+    function rude() {
+      document.getElementById( "myCanvas" ).style.backgroundColor = "black";
+      ghost1.height -= 10;
+      ghost1.width -= 10;
+      window.alert("Ouch! Watch Where You're Going!");
+    }
 
   });
 });
