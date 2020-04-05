2e4ea6639f602b7bc1662f3016a93f8421b59100
Eli Watts <eliwatts13@gmail.com>
2020-04-05 15:26:26 -0600

    assignment meets minimum requirements

--- hw-11/script/amoeba.js
+++ hw-11/script/amoeba.js
@@ -48,3 +48,47 @@ class Square{
         return this.color;
     }
 }
+
+class Wall{
+
+    constructor(xCoord,yCoord,objectHeight,objectWidth, color)
+    {
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.objectHeight = objectHeight;
+        this.objectWidth = objectWidth;
+        this.color = color;
+    }
+
+    get x()
+    {
+        return this.xCoord;
+    }
+
+    set x(value)
+    {
+        this.xCoord = value;
+    }
+    get y()
+    {
+        return this.yCoord;
+    }
+    set y(value)
+    {
+        this.yCoord = value;
+    }
+
+    get height()
+    {
+        return this.objectHeight;
+    }
+
+    get width()
+    {
+        return this.objectWidth;
+    }
+    get mainColor()
+    {
+        return this.color;
+    }
+}
--- hw-11/script/game.js
+++ hw-11/script/game.js
@@ -13,7 +13,8 @@ var square1, square2;
 var direction;
 var questions;
 var squareArray = [];
-var lives = 3;
+var lives = 0;
+var wallArray = [];
 
 $(document).ready(function(){
 
@@ -32,16 +33,23 @@ function setup() {
     ctx = canvas.getContext("2d");
 
     // create two objects
-    square1 = new Square(100,100,20,20,"#0000FF");
+    square1 = new Square(50,200,20,20,"#0000FF");
     square2 = new Square(400,400,100,100,"#00FF00");
+    wall1 = new Wall( 200 , 200 , 20 , 20 , "red" );
     //console.log("hi");
     $.getJSON("../script/amoebas.json", function(data) {
         for(var i = 0; i < data.squares.length; i++) {
-            squareArray.push(new Square(data.squares[i].x,data.squares[i].y, data.squares[i].h, data.squares[i].w, data.squares[i].color));
+            squareArray.push(new Square(data.squares[i].x , data.squares[i].y , data.squares[i].h , data.squares[i].w , data.squares[i].color));
         }
 
-        drawSquare();
+    $.getJSON("../script/walls.json" , function( info ) {
+      for( var k = 0; k < info.walls.length; k++ ) {
+        wallArray.push( new Wall( info.walls[k].x , info.walls[k].y , info.walls[k].h , info.walls[k].w , info.walls[k].color ));
+      }
 
+        drawSquare();
+        //console.log(data.squares[0]);
+      });
     });
 }
 
@@ -63,26 +71,37 @@ function getKey(event) {
     }
 
 
-    var test = hasCollided(square1,square2);
+    var test = hasCollided( square1 , square2 );
     var test2 = false;
+    var test3 = hasCollided( square1 , wall1 );
+    var test4 = false;
+
     for(var i = 0; i < squareArray.length; i++) {
-        test2 = hasCollided(square1,squareArray[i]);
+        test2 = hasCollided( square1 , squareArray[i] );
         if(test2 == true) {
             break;
         }
-
         //console.log(test2);
-    } if(test || test2) {
-        //lives--;
-        if(direction == "left") {
-            moveRight();
-        } else if(direction == "right") {
-            moveLeft();
-        } else if(direction == "up") {
-            moveDown();
-        } else if(direction == "down") {
-            moveUp();
-        }
+    } if(test || test2 ) {
+        lives ++;
+        squareArray.splice( i , 1 );
+    }
+    for( var l = 0; l < wallArray.length; l++ ) {
+      test3 = hasCollided( square1 , wallArray[l] );
+      if( test3 == true ) {
+        break;
+      }
+    } if( test3 || test4 ) {
+      //console.log("ow");
+      if(direction == "left") {
+          moveRight();
+      } else if(direction == "right") {
+          moveLeft();
+      } else if(direction == "up") {
+          moveDown();
+      } else if(direction == "down") {
+          moveUp();
+      }
     }
     someBoundaries(square1);
     drawSquare();
@@ -113,8 +132,16 @@ function drawSquare() {
         ctx.fillRect(squareArray[i].x, squareArray[i].y, squareArray[i].width, squareArray[i].height);
     }
 
-    //ctx.font = "30px Arial";
-    //ctx.fillText("Lives: " + lives, 10, 50);
+    ctx.fillStyle = wall1.mainColor;
+    ctx.fillRect(wall1.x, wall1.y, wall1.width, wall1.height);
+    for(var j = 0; j < wallArray.length; j++) {
+        ctx.fillStyle = wallArray[j].mainColor;
+        ctx.fillRect(wallArray[j].x, wallArray[j].y, wallArray[j].width, wallArray[j].height);
+    }
+
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText("Points: " + lives, 10, 50);
 
 }
 
@@ -141,3 +168,10 @@ function someBoundaries( object ) {
     moveLeft();
   }
 }
+
+/*function omNom( object1 , object2 ) {
+  if( object1.h > object2.h ) {
+    object1.h += 10;
+    object1.w += 10;
+  }
+}*/
