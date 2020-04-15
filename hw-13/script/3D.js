ad0f2085eba9ba5903501a9bda433524c42b2b33
Eli Watts <eliwatts13@gmail.com>
2020-04-15 06:45:25 -0600

    tried some things

--- hw-13/script/3D.js
+++ hw-13/script/3D.js
@@ -5,12 +5,40 @@ Date: 04/14/2020
 Purpose: MART 441 Homework Assignment #13
 */
 
-var scene = new THREE.Scene()
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+}
+animate();
+
+
+
+/*var scene = new THREE.Scene()
         scene.overrideMaterial = new THREE.MeshBasicMaterial({
             color: 'green'
         });
         scene.background = new THREE.Color('grey');
-        var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000);
+        var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000)
         var renderer = new THREE.WebGLRenderer()
         camera.position.x = 100
         camera.position.y = -100
@@ -49,4 +77,4 @@ var scene = new THREE.Scene()
             renderer.render(scene, camera);
         };
 
-        render();
+        render();*/
