2a8d30bbcbba63913c91379354e4b25c2ccac740
Eli Watts <eliwatts13@gmail.com>
2020-04-19 08:45:38 -0600

    Finished hw-13

--- hw-13/readme.md
+++ hw-13/readme.md
@@ -3,8 +3,20 @@
 # Homework Assignment 13
 
 ## Intro
+This week’s assignment was surprisingly difficult. Even trying to get some of this week’s example code to work took a lot of trial and error. Part of the reason for that may be that I have never worked with the three.js library and there is a lot of syntax that I did not understand. I was eventually able to put something together which meets at least some of the homework submission requirements, but I feel like all I did was combine two pieces of the example code and then make very slight modifications to it.
 
 ## What Worked?
 
+My understanding of three.js is that it requires three major components: the scene, the camera, and the renderer. In the scene, objects can be displayed, and events can happen. The camera works much like a physical camera and displays whatever objects or movement it is currently pointed at. Finally, the renderer draws or updates objects within the scene.
+
+My homework assignment works mainly by creating new object instances of classes which have been defined within the three.js library, like the camera, the scene, or the renderer. Once instances of these class-based objects have been created, the new object instances can be manipulated. For example, after creating new instances of the `THREE.BoxGeometry();` I could then set its x, y, and z position coordinates. Later, I could then update those coordinates to create a spinning effect.
+
+As I planned on working with multiple `BoxGeometry();` objects, I decided to group them together using an instance of the `THREE.Group();` class. In theory, this would allow me to be able to apply a function once to an entire group rather than needing to apply a function multiple times to multiple objects.
 
 ## What Didn’t Work?
+
+As stated in the intro, I feel like a lot did not work well for me this week and that I am turning in the barest minimum amount work for this assignment. I found it very difficult to do basic tasks like positioning objects. Looking for instructions on how to do things often returned results which included extra complexities that I did not understand. While I did get some things to work, I think I need a lot more experience with the three.js library before I feel even remotely comfortable with how things work.
+
+Everything in my scene is purple. I am assuming this is because the `scene.overrideMaterial();` has a value of purple which all my other objects inherit. In P5.js there was a way to isolate chunks of code from other chunks using the `push();` and `pop();` methods. I am assuming there is something similar in three.js, but I don’t know what it is.
+
+When I tried writing the `animate();` function to act on my `cubeGroup();` it ended up causing `cube2` to fly around wildly. I think this is because `cube2` was not centered on the x, y, and z axis. I am also assuming there is a way to transform where the axis' vertices intersect, like in P5.js, but again, I’m not sure how to do it.
--- hw-13/script/3D.js
+++ hw-13/script/3D.js
@@ -5,55 +5,61 @@ Date: 04/14/2020
 Purpose: MART 441 Homework Assignment #13
 */
 
-/*var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
-});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-function animate() {
-    requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-}
-animate();*/
-
-
-
-var scene = new THREE.Scene()
+//Creates new scene
+var scene = new THREE.Scene();
+        //Overrides any default mesh
+        //I think this is also causing my cubes to be purple rather than green
+        //Couldn't find any alternatives
         scene.overrideMaterial = new THREE.MeshBasicMaterial({
-            color: 'green'
+            color: '#6a0dad'
         });
+
+        //Sets scene background to grey
         scene.background = new THREE.Color('grey');
-        var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000)
-        var renderer = new THREE.WebGLRenderer()
-        camera.position.x = 100
-        camera.position.y = -100
-        camera.position.z = 590
 
+        //Initializes new variable and assigns new geometry as a value
+        var geoBox = new THREE.BoxGeometry( 2 , 2 , 2 );
+        //Initializes new variable and assigns new mesh as value
+        //Doesn't work as it is overrided by previous scene.overrideMaterial
+        var material = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        });
 
-        renderer.setClearColor(0xdddddd)
-        renderer.setSize(window.innerWidth, window.innerHeight)
+        //Initializes new cube and passes in geoBox and material variables as arguments to set size and color
+        var cube1 = new THREE.Mesh(geoBox , material);
+        //positoins cube at x, y, and z coordinates
+        cube1.position.set( 0 , 0 , 0 );
 
-        document.body.appendChild(renderer.domElement)
+        //Same process here as for cube1
+        var cube2 = new THREE.Mesh(geoBox , material);
+        cube2.position.set( 40 , 0 , 0 );
+
+        //Creates new group in which cube1 and cube2 are stored
+        //Theoritically allows functions to be more easily applied across large groups of objects
+        var cubeGroup = new THREE.Group();
+        cubeGroup.add(cube1);
+        cubeGroup.add(cube2);
+        scene.add(cubeGroup);
+
+        //Adds new camera object to scene and positions it
+        //Assigns THREE>WebGLRenderer to renderer variable
+        var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000);
+        var renderer = new THREE.WebGLRenderer();
+        camera.position.x = 20;
+        camera.position.y = 0;
+        camera.position.z = 50;
+
+        //Clears frame to prevent moving shapes from smearing
+        renderer.setClearColor(0xdddddd);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        //Creates new GLTFLoader which imports 3D objects
+        document.body.appendChild(renderer.domElement);
         var loader = new THREE.GLTFLoader();
 
-        // change to the name of your glb
+        //Loads 3D object stored in images folder
         loader.load('../images/lowPolyTrees.glb', function(gltf) {
-
+            //Adds gltf object to scene
             scene.add(gltf.scene);
 
         }, undefined, function(error) {
@@ -61,20 +67,41 @@ var scene = new THREE.Scene()
             console.error(error);
 
         });
-        var rotation = 0
+        //Assigns value of 0 to rotation variable
+        var rotation = 0;
 
+        //Spins camera by updating its z and x values
         function spinCamera() {
-            rotation += 0.001
+            rotation += 0.001;
             camera.position.z = Math.sin(rotation) * 80;
             camera.position.x = Math.cos(rotation) * 80;
-            camera.lookAt(scene.position)
+            camera.lookAt(scene.position);
         }
+
+        //Rotates object by updating their x and y values
+        function animate() {
+            requestAnimationFrame( animate );
+            cube1.rotation.x += 0.01;
+            cube1.rotation.y += 0.01;
+
+            cube2.rotation.x += 0.01;
+            cube2.rotation.y += 0.01;
+
+            //Correctly rotates cube1 but causes cube 2 to fly around wildly as it is not positioned at 0, 0, 0
+            //cubeGroup.rotation.x += 0.01;
+            //cubeGroup.rotation.y += 0.01;
+
+            renderer.render( scene, camera );
+        }
+
+        //Renders scene
         var render = function() {
 
             requestAnimationFrame(render);
-            spinCamera();
 
             renderer.render(scene, camera);
         };
 
+        //Calls animate function and render function
+        animate();
         render();
